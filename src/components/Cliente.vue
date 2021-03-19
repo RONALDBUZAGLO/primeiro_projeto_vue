55a0813b9b6cb807b714d018ae7ab2ef0f87d23e
RONALDBUZAGLO <senseironald@gmail.com>
2021-03-19 16:38:39 -0400

    primeiro projeto

---
+++ src/components/Cliente.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="cliente">
+       <h4>Nome: {{nome}}</h4>
+       <hr>
+       <p> {{descricao}} </p>
+       <hr>
+       <p>Número: {{numero}} </p>
+       <p>E-mail: {{email}} </p>
+       <p>Idade: {{idade}} </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            
+            descricao:"Lorem ipsumasdljkasldkjasldkj",
+            numero: "asdasdasd",
+            email: "asdlkajsdlkj@lkjsdlajk",
+            idade: 30,
+        }
+    },
+    props:{
+        nome: String,
+    }
+}
+</script>
+
+<style scoped>
+#cliente{
+    background: #B8E9FF;
+    margin: 1% auto;
+    width: fit-content;
+    padding: 1% 10%;
+}
+
+</style>
+
