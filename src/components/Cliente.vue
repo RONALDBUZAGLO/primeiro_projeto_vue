4c6af6305d6e9941305d78e49b3e1ca4ea1f5eef
ronaldbuzaglo <senseironald@gmail.com>
2021-03-20 12:14:35 -0400

    trabalhando em casa

---
+++ src/components/Cliente.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="cliente">
+       <h4>Nome: {{nome}}</h4>
+       <hr>
+       <p> {{descricao}} </p>
+       <hr>
+       <p>Número: {{numero}} </p>
+       <p>E-mail: {{email}} </p>
+       <p>Idade: {{idade}} </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            
+            descricao:"Lorem ipsumasdljkasldkjasldkj",
+            numero: "asdasdasd",
+            email: "asdlkajsdlkj@lkjsdlajk",
+            idade: 30,
+        }
+    },
+    props:{
+        nome: String,
+    }
+}
+</script>
+
+<style scoped>
+#cliente{
+    background: #B8E9FF;
+    margin: 1% auto;
+    width: fit-content;
+    padding: 1% 10%;
+    border-radius: 5%;
+}
+</style>
